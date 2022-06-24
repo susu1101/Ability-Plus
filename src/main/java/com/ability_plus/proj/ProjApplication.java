5a2c651a6635def4b4ba9ac7727ed21aed16f075
junxu su <715182333@qq.com>
2022-06-25 04:56:33 +1000

    新增： 一些配置上的修改，此版本还有一些问题。 不识别application内的端口配置，待修复

--- src/main/java/com/ability_plus/proj/ProjApplication.java
+++ src/main/java/com/ability_plus/proj/ProjApplication.java
@@ -1,9 +1,12 @@
 package com.ability_plus.proj;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication
 public class ProjApplication {
 
     public static void main(String[] args) {
