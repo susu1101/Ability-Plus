3abfa790a9be892f33a045d3a34011248412e134
junxu su <715182333@qq.com>
2022-07-16 04:55:07 +1000

    点赞系统

--- ability-plus-service/src/main/java/com/ability_plus/user/controller/UserProposalLikeRecordController.java
+++ ability-plus-service/src/main/java/com/ability_plus/user/controller/UserProposalLikeRecordController.java
@@ -1,8 +1,18 @@
 package com.ability_plus.user.controller;
 
 
+import com.ability_plus.user.service.IUserProposalLikeRecordService;
+import com.ability_plus.utils.RestResponse;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -15,5 +25,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user_proposal_like_record")
 public class UserProposalLikeRecordController {
+    @Autowired
+    IUserProposalLikeRecordService userProposalLikeRecordService;
+    @PostMapping("/like")
+    @ApiOperation("like")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "proposalId", value = "proposalId", required = true),
+    })
+    public RestResponse like(@RequestParam Integer proposalId, HttpServletRequest http){
+        userProposalLikeRecordService.like(proposalId,http);
+        return RestResponse.success();
+    }
+    @PostMapping("/unlike")
+    @ApiOperation("unlike")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "proposalId", value = "proposalId", required = true),
+    })
+    public RestResponse unlike(@RequestParam Integer proposalId, HttpServletRequest http){
+        userProposalLikeRecordService.unlike(proposalId,http);
+        return RestResponse.success();
+    }
+    @PostMapping("/get_like_num")
+    @ApiOperation("get_like_num")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "proposalId", value = "proposalId", required = true),
+    })
+    public RestResponse<Integer> getLikeNum(@RequestParam Integer proposalId){
+        return RestResponse.success(userProposalLikeRecordService.getLikeNum(proposalId));
+    }
 
 }
--- ability-plus-service/src/main/java/com/ability_plus/user/service/IUserProposalLikeRecordService.java
+++ ability-plus-service/src/main/java/com/ability_plus/user/service/IUserProposalLikeRecordService.java
@@ -3,6 +3,9 @@ package com.ability_plus.user.service;
 
 import com.ability_plus.user.entity.UserProposalLikeRecord;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -13,5 +16,26 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-06-30
  */
 public interface IUserProposalLikeRecordService extends IService<UserProposalLikeRecord> {
+    /**
+     * 点赞
+     * @param proposalId
+     * @param http
+     */
+    public void like(Integer proposalId, HttpServletRequest http);
+    /**
+     * 取消点赞
+     * @param proposalId
+     * @param http
+     */
+    public void unlike(Integer proposalId, HttpServletRequest http);
+
+
+    /**
+     * 获取点赞数
+     * @param proposalId
+     * @param http
+     * @return
+     */
+    public Integer getLikeNum(Integer proposalId);
 
 }
