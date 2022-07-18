2008cda557f20c045fa156c43f2fcb732814d239
junxu su <715182333@qq.com>
2022-07-19 04:42:58 +1000

    定时任务修改proposal状态

--- ability-plus-service/src/main/java/com/ability_plus/system/ProjectStatusCheckTask.java
+++ ability-plus-service/src/main/java/com/ability_plus/system/ProjectStatusCheckTask.java
@@ -3,6 +3,9 @@ package com.ability_plus.system;
 import com.ability_plus.projectRequest.entity.ProjectRequest;
 import com.ability_plus.projectRequest.entity.ProjectRequestStatus;
 import com.ability_plus.projectRequest.service.IProjectRequestService;
+import com.ability_plus.proposal.entity.Proposal;
+import com.ability_plus.proposal.entity.ProposalStatus;
+import com.ability_plus.proposal.service.IProposalService;
 import com.ability_plus.utils.TimeUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
@@ -10,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author sjx
@@ -20,7 +26,11 @@ public class ProjectStatusCheckTask {
 
     @Autowired
     IProjectRequestService projectRequestService;
+    @Autowired
+    IProposalService proposalService;
+
     @Scheduled(cron = "0 0/1 * * * ?")
+    @Transactional(rollbackFor = Exception.class)
     public void checkStatusTask(){
         checkProposalDue();
         checkSolutionDue();
@@ -37,14 +47,38 @@ public class ProjectStatusCheckTask {
         projectRequestService.update(updateData,wrapper);
     }
 
+
     private void checkProposalDue() {
-        UpdateWrapper<ProjectRequest> wrapper = new UpdateWrapper<>();
+        QueryWrapper<ProjectRequest> wrapper = new QueryWrapper<>();
         long nowTime = TimeUtils.getTimeStamp();
-        //not draft
         wrapper.eq("status", ProjectRequestStatus.OPEN_FOR_PROPOSAL);
         wrapper.lt("proposal_ddl",nowTime);
-        ProjectRequest updateData = new ProjectRequest();
-        updateData.setStatus(ProjectRequestStatus.APPROVING);
-        projectRequestService.update(updateData,wrapper);
+        List<ProjectRequest> projectNeedChange = projectRequestService.list(wrapper);
+        if (projectNeedChange.size()==0){
+            return;
+        }
+        for (ProjectRequest project:projectNeedChange){
+            UpdateWrapper<Proposal> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.ne("status", ProposalStatus.DRAFT);
+            updateWrapper.inSql("id","select id proposal_id from project_proposal_record where project_id="+project.getId().toString());
+
+            Proposal proposal = new Proposal();
+            proposal.setStatus(ProposalStatus.APPROVING);
+            proposalService.update(proposal,updateWrapper);
+
+            project.setStatus(ProjectRequestStatus.APPROVING);
+            projectRequestService.updateById(project);
+        }
+
+
+//        UpdateWrapper<ProjectRequest> wrapper = new UpdateWrapper<>();
+//        long nowTime = TimeUtils.getTimeStamp();
+//        //not draft
+//        wrapper.eq("status", ProjectRequestStatus.OPEN_FOR_PROPOSAL);
+//        wrapper.lt("proposal_ddl",nowTime);
+//        ProjectRequest updateData = new ProjectRequest();
+//        updateData.setStatus(ProjectRequestStatus.APPROVING);
+//        projectRequestService.update(updateData,wrapper);
+//
     }
 }
