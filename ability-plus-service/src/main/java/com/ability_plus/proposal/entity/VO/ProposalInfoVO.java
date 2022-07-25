8757d2b66bd63d85b04535ff2cf267c5601a317c
junxu su <715182333@qq.com>
2022-07-25 23:26:07 +1000

    forum-edit

--- ability-plus-service/src/main/java/com/ability_plus/proposal/entity/VO/ProposalInfoVO.java
+++ ability-plus-service/src/main/java/com/ability_plus/proposal/entity/VO/ProposalInfoVO.java
@@ -9,6 +9,6 @@ import lombok.EqualsAndHashCode;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class ProposalInfoVO extends CardUtils {
+public class ProposalInfoVO extends ProposalCard {
     private String status;
 }
